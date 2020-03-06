1297252213c052363c89e8656b66768d9fc22b7c
jasercion <joseph.a.asercion@nasa.gov>
2020-03-05 21:19:28 -0500

    refactor code, remove data dir

--- adc2tte.py
+++ adc2tte.py
@@ -1,58 +1,84 @@
 from astropy.io import fits
 from astropy.table import Table
 import numpy as np
+import argparse
 import sys
 
-def bintablehdu_constructor(data, hduname):
-    table = Table(rows=data[0], names=data[1])
+def parseargs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("infile", help="ACD count file to process", type=argparse.FileType('r'))
+    parser.add_argument("-o", "--outfile", help="Output filename", default="burstcube_tte.fits")
+
+    args = parser.parse_args()
+    return args
+
+def bintablehdu_constructor(data, colnames, coltypes, hduname):
+    table = Table(rows=data, names=colnames, dtype=coltypes)
     bintablehdu = fits.BinTableHDU(table, name=hduname)
     return bintablehdu
 
-file = sys.argv[1]
-f = open(file,"r")
-lines = f.readlines()
-input = []
+if __name__ == '__main__':
+    # Read ADC output text file
+    # Expected Format:
+    ##############################################
+    # Detector  Timestamp  Baseline  Pulseheight #
+    ##############################################
+    args = parseargs()
+    outfile = args.outfile
+    f = args.infile
+    
+    lines = f.readlines()
+    input = []
+    
+    for x in lines:
+        input.append(tuple((x.split(' ')[0],
+                            x.split(' ')[1],
+                            x.split(' ')[2],
+                            x.split(' ')[3].strip())))
+    f.close()
 
-for x in lines:
-    input.append(tuple((x.split(' ')[0],
-                        x.split(' ')[1],
-                        x.split(' ')[2],
-                        x.split(' ')[3].strip())))
+    elements =  input.pop(0)
+    
+    tstart = (input[0])[1]
+    tstop = (input[-1])[1]
 
-f.close()
-
-scaledpulse = []
-
-elements = input.pop(0)
-
-for x in input:
-    scaledpulse.append([int(x[1]),int(x[3])-int(x[2])])
-
-channels = []
-tstart = (input[0])[1]
-tstop = (input[-1])[1]
-emin = 0
-emax = 250
-i = 1
-
-while i < 17:
-    channels.append([i,emin,emax])
-    emin = emax+1
-    emax = emax+250
-    i += 1
-
-eventpairs = []
-
-for x in scaledpulse:
-    for y in channels:
-        if x[1] in range(y[1],y[2]+1,1):
-            eventpairs.append([x[0],y[0]])
-
-primary_hdu = fits.PrimaryHDU()
-ebounds_hdu = bintablehdu_constructor([channels,('CHANNEL','E_MIN','E_MAX')], 'EBOUNDS')
-events_hdu = bintablehdu_constructor([eventpairs,('EVENT','PHA')], 'EVENTS')
-gti_hdu = bintablehdu_constructor([[[tstart,tstop]],('TSTART','TSTOP')], 'GTI')
-
-hdulist = fits.HDUList([primary_hdu,ebounds_hdu,events_hdu,gti_hdu])
-
-hdulist.writeto('burstcubette.fits')
+    # Extract Timestamps and scaled pulseheights from each event
+    # and store in the scaledpulse list
+    scaledpulse = []
+    
+    for x in input:
+        scaledpulse.append([int(x[1]),int(x[3])-int(x[2])])
+        
+    # Define Energy Channels
+    # Current: 16 channels hardcoded in increments
+    # of 250
+    channels = []
+    emin = 0
+    emax = 250
+    i = 1
+    
+    while i < 17:
+        channels.append([i,emin,emax])
+        emin = emax+1
+        emax = emax+250
+        i += 1
+        
+    # Match each scaled pulse with an energy channel range
+    # and create a list of timestamp/channel pairs
+    eventpairs = []
+    
+    for x in scaledpulse:
+        for y in channels:
+            if x[1] in range(y[1],y[2]+1,1):
+                eventpairs.append([x[0],y[0]])
+                
+    # Construct the output HDUs
+    primary_hdu = fits.PrimaryHDU()
+    ebounds_hdu = bintablehdu_constructor(channels,('CHANNEL','E_MIN','E_MAX'), ('short','float','float'),'EBOUNDS')
+    events_hdu = bintablehdu_constructor(eventpairs,('EVENT','PHA'), ('double','short'),'EVENTS')
+    gti_hdu = bintablehdu_constructor([[tstart,tstop]],('TSTART','TSTOP'), ('double','double'), 'GTI')
+    hdulist = fits.HDUList([primary_hdu,ebounds_hdu,events_hdu,gti_hdu])
+    
+    # Write out the tte file
+    hdulist.writeto(outfile)
